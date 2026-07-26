ebee1d3864ac0667ec9eafd93e84528d2c54ffb8
Atulyaks <atulyasingh2005@gmail.com>
2026-07-26 14:43:45 +0530

    Add DB port for Railway deployment

--- server/server.js
+++ server/server.js
@@ -32,3 +32,19 @@ sequelize
     console.error("❌ Database connection failed");
     console.error(err);
   });
+  const { Sequelize } = require("sequelize");
+require("dotenv").config();
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "mysql",
+    logging: false,
+  }
+);
+
+module.exports = sequelize;
